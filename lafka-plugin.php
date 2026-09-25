2fbba160664465e9dce5767074cb5a352b3fbcfe
Raghav Gupta <hello@raghavgupta.ca>
2026-09-25 17:39:07 -0300

    chore(release): bump version to 10.3.0

--- lafka-plugin.php
+++ lafka-plugin.php
@@ -3,7 +3,7 @@
 	Plugin Name: Lafka Plugin
 	Plugin URI: https://github.com/setkernel/lafka-plugin
 	Description: Companion plugin for the Lafka WooCommerce theme. Originally by theAlThemist, now community-maintained.
-	Version: 10.2.1
+	Version: 10.3.0
 	Author: theAlThemist, Contributors
 	Author URI: https://github.com/setkernel/lafka-plugin
 	Requires at least: 6.6
